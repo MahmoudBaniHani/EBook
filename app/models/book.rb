fac41a393e6f97ccb92f19290f9cd236df3026a4
MahmoudBaniHani <mbanihani@everconnect>
2022-11-21 21:40:07 +0300

    upload after insert validates

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :name ,:presence=>true,length: { minimum: 30 ,minimum:1 }
+  validates :author ,:presence=>true
+  validates :rating ,:inclusion=>{in:[1,2,3,4,5],:message=>"Rate is from 1 to 5"}
+  # validates :publish_year
 end
